3ea4f6e5a7142fc5d3d03e9680c2a8323a55a385
daired <hady@khalifa.de>
2013-04-17 20:59:39 +0200

    modified css layout with fixed header; Template layout for wall.php, profile.php, gallery.php

--- wall.php
+++ wall.php
@@ -1,11 +1,49 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" 
+	"http://www.w3.org/TR/html4/strict.dtd">
 <html>
-<head>
-<title>Scribb'it - Wall</title>
-<script type="text/javascript" src="wall.js"></script>
-<link rel="stylesheet" type="text/css" href="wall.css">
-</head>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
 
-<body>
+		<link rel="stylesheet" type="text/css" href="dashboard.css">
+		<script type="text/javascript" src="gallery.js"></script>
+		<title>Scribbit - Wall</title>
+	</head>
 
-</body>
+	<body>
+			<div id="site">
+			<div id="header">
+
+				<div id="navigation">
+					<?php
+					echo '<span><a href="'.path.'/wall">WALL</a></span>
+							<span><a href="'.path.'/gallery">GALLERY</a></span>
+							<span><a href="'.path.'/profile">PROFILE</a></span>
+							<span><a href="'.path.'/logout">LOGOUT</a></span>';
+					?>
+				</div>
+
+				<div id="logo">
+					<?php
+					echo '<a href="'.path.'/"><</a>';
+					?>
+				</div>
+
+			</div>
+
+				<div id="content">
+					<br>
+					<table border="0">
+						<tr>
+							<td>1</td>
+							<td>2</td>
+							<td>3</td>
+						</tr>
+					</table>
+				
+		<h1> Hier steht eine Wand</h1>
+					
+					<br>
+				</div>
+			</div>
+	</body>
 </html>
