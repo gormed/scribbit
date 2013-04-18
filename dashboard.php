b4c48448cdd8c156df8ebcae7c4e352ff3d2db63
gormed <hans.ferchland@gmx.de>
2013-04-18 12:06:52 +0100

    merged

--- dashboard.php
+++ dashboard.php
@@ -2,50 +2,52 @@
 	"http://www.w3.org/TR/html4/strict.dtd">
 <html>
 	<head>
-		<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
 
 		<link rel="stylesheet" type="text/css" href="dashboard.css">
 		<title>Dashboard</title>
 	</head>
 
 	<body>
-		<div id="back">
-			<?php
-			echo '<a href="'.path.'/"><</a>';
-			?>
-		</div>
+			<div id="site">
+			<div id="header">
 
-		<div id="logo">
-			Dashboard
-		</div>
+				<div id="navigation">
+					<?php
+					echo '<span><a href="'.path.'/wall">WALL</a></span>
+							<span><a href="'.path.'/gallery">GALLERY</a></span>
+							<span><a href="'.path.'/profile">PROFILE</a></span>
+							<span><a href="'.path.'/logout">LOGOUT</a></span>';
+					?>
+				</div>
 
-		<div id="navigation">
-			<?php
-			echo '<li><a href="'.path.'/wall">WALL</a></li>';
-			echo '<li><a href="'.path.'/gallery">GALLERY</a></li>';
-			echo '<li><a href="'.path.'/profile">PROFILE</a></li>';
-			echo '<li><a href="'.path.'/logout">LOGOUT</a></li>';
-			?>
-		</div>
+				<div id="logo">
+					<?php
+					echo '<a href="'.path.'/"><</a>';
+					?>
+				</div>
 
-		<div id="content">
-			<br>
-			<table border="0">
-				<tr>
-					<td>1</td>
-					<td>2</td>
-					<td>3</td>
-				</tr>
-			</table>
-			<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-			</p>
-			<br>
-		</div>
+			</div>
+
+				<div id="content">
+					<br>
+					<table border="0">
+						<tr>
+							<td>1</td>
+							<td>2</td>
+							<td>3</td>
+						</tr>
+					</table>
+					<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+					<br>
+				</div>
+			</div>
 	</body>
 </html>
--- profile.php
+++ profile.php
@@ -1,11 +1,60 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" 
+	"http://www.w3.org/TR/html4/strict.dtd">
 <html>
-<head>
-<title>Scribb'it - Profile</title>
-<script type="text/javascript" src="profile.js"></script>
-<link rel="stylesheet" type="text/css" href="ressources/css/profile.css">
-</head>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+		<link rel="stylesheet" type="text/css" href="dashboard.css">
+		<script type="text/javascript" src="gallery.js"></script>
+		<title>Scribbit - Profil</title>
+	</head>
 
-<body>
+	<body>
+			<div id="site">
+			<div id="header">
 
-</body>
+				<div id="navigation">
+					<?php
+					echo '<span><a href="'.path.'/wall">WALL</a></span>
+							<span><a href="'.path.'/gallery">GALLERY</a></span>
+							<span><a href="'.path.'/profile">PROFILE</a></span>
+							<span><a href="'.path.'/logout">LOGOUT</a></span>';
+					?>
+				</div>
+
+				<div id="logo">
+					<?php
+					echo '<a href="'.path.'/"><</a>';
+					?>
+				</div>
+
+			</div>
+
+				<div id="content">
+					
+					<div>
+						<br>
+						<br>
+						Meine eigenen Scribbles kronologisch sortiert
+						<br>
+						<br>
+					</div>
+
+					<div>
+						<br>
+						<br>
+						Scribbles meiner Freunde kronologisch sortiert
+						<br>
+						<br>
+					</div>
+
+					<div>
+						<br>
+						<br>
+						Scribbles meiner Favoriten kronologisch sortiert
+						<br>
+						<br>
+					</div>
+				</div>
+			</div>
+	</body>
 </html>
