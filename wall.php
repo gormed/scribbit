76678b8155efbba364a14309879a4e34a70d4055
daired <hady@khalifa.de>
2013-05-05 20:55:44 +0200

    test openlayers implementation in wall.php

--- wall.php
+++ wall.php
@@ -8,7 +8,7 @@ require_once 'header.php';
 		<meta http-equiv="content-type" content="text/html; charset=utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 		<meta name="apple-mobile-web-app-capable" content="yes">
-		<link rel="stylesheet" type="text/css" href="ressources/css/headerSearch.css">		
+		<link rel="stylesheet" type="text/css" href="ressources/css/headerSearch.css">      
 		<link rel="stylesheet" type="text/css" href="ressources/css/wall.css">
 		<link rel="stylesheet" type="text/css" href="ressources/css/openLayersTheme.css">
 		<style type="text/css">
@@ -26,121 +26,197 @@ require_once 'header.php';
 		var scribbles = {};
 		var scribbleLayers = {};
 
-		// function addElement(ni) {
-		// 	var numi = document.getElementById('theValue');
-		// 	var num = (document.getElementById('theValue').value -1)+ 2;
-
-		// 	numi.value = num;
-		// 	var newdiv = document.createElement('div');
-		// 	var divIdName = 'wall'+num+'Div';
-
-		// 	newdiv.setAttribute('id',divIdName);
-		// 	newdiv.innerHTML = 'Element Number '+num+' has been added! <a href=\'#\' onclick=\'removeElement('+divIdName+')\'>Remove the div "'+divIdName+'"</a>';
-		// 	ni.appendChild(newdiv);
-		// 	return newdiv;
-		// }
-
-		// function removeElement(divNum, d) {
-		// 	var olddiv = document.getElementById(divNum);
-		// 	d.removeChild(olddiv);
-		// }
-
-		function loadScribbles () {
-
-		// <?php 
-
-		// 	if (!$loggedIn) {
-		// 		exit();
-		// 	}
-		// 	echo "var path = '".path."';";
-		// 	$sql = "SELECT `scribbleid`, `path` FROM `scribbles` LIMIT 0, 40 ";
-		// 	$result = $mysqli->query($sql);
-		// 	while ($row = $result->fetch_array(MYSQLI_NUM)) {
-		// 		echo 'scribbles['.$row[0]."] = '".$row[1]."';";
-		// 	}
-		// ?>
-		// 	// var wall = document.getElementById('wall');
-		// 	// var row = document.createElement('div');
-		// 	// row.setAttribute('class','row');
-		// 	// wall.appendChild(row);
-		// 	// var temp;
-
-		// 	for (var k in scribbles) {
-		// 		// use hasOwnProperty to filter out keys from the Object.prototype
-		// 		if (scribbles.hasOwnProperty(k)) {
-		// 			// temp = document.createElement('div');
-		// 			// temp.setAttribute('class', 'cell');
-		// 			// temp.innerHTML = '<a href="'+path+'/view"><img src="'+path+'/'+scribbles[k]+'"></a><br>';
-		// 			// row.appendChild(temp);
-		// 			//<?php echo '<div class="cell"><a href="'.path.'/view"><img src="ressources/img/template.gif"></a></div>' ?>
-		// 			//alert('key is: ' + k + ', value is: ' + scribbles[k]);
-
-		// 			var graphic = new OpenLayers.Layer.Image(
-		// 			'scribble_'+k+'',
-		// 			''+path+'/'+scribbles[k]+'',
-		// 			new OpenLayers.Bounds(-200, -150, 200, 150),
-		// 			new OpenLayers.Size(400, 300),
-		// 			{numZoomLevels: 3}
-		// 			);
-		// 			var scribbleLayers[k] = graphic;
-		// 		}
-		// 	}
 
 
 
-		// 	graphic.events.on({
-		// 		loadstart: function() {
-		// 			OpenLayers.Console.log("loadstart");
-		// 		},
-		// 		loadend: function() {
-		// 			OpenLayers.Console.log("loadend");
-		// 		}
-		// 	});
+	
+			function addElement(ni) {
+				var numi = document.getElementById('theValue');
+				var num = (document.getElementById('theValue').value -1)+ 2;
+
+				numi.value = num;
+				var newdiv = document.createElement('div');
+				var divIdName = 'wall'+num+'Div';
+
+				newdiv.setAttribute('id',divIdName);
+				newdiv.innerHTML = 'Element Number '+num+' has been added! <a href=\'#\' onclick=\'removeElement('+divIdName+')\'>Remove the div "'+divIdName+'"</a>';
+				ni.appendChild(newdiv);
+				return newdiv;
+			}
+
+			function removeElement(divNum, d) {
+				var olddiv = document.getElementById(divNum);
+				d.removeChild(olddiv);
+			}
+
+		// function loadScribbles () {
 
 
 
-			var points = new OpenLayers.Layer.PointGrid({
-				isBaseLayer: true, dx: 15, dy: 15
-			});
 
-			var map = new OpenLayers.Map({
-				div: "map",
-				layers: [points],
-				center: new OpenLayers.LonLat(0, 0),
-				zoom: 2
-			});
+			// for (var k in scribbles) {
+			// 	// use hasOwnProperty to filter out keys from the Object.prototype
+			// 	if (scribbles.hasOwnProperty(k)) {
+			// 		// temp = document.createElement('div');
+			// 		// temp.setAttribute('class', 'cell');
+			// 		// temp.innerHTML = '<a href="'+path+'/view"><img src="'+path+'/'+scribbles[k]+'"></a><br>';
+			// 		// row.appendChild(temp);
+			// 		//<?php echo '<div class="cell"><a href="'.path.'/view"><img src="ressources/img/template.gif"></a></div>' ?>
+			// 		//alert('key is: ' + k + ', value is: ' + scribbles[k]);
 
-			var rotation = document.getElementById("rotation");
-			rotation.value = String(points.rotation);
-			rotation.onchange = function() {
-				points.setRotation(Number(rotation.value));
-			};
-
-			var dx = document.getElementById("dx");
-			var dy = document.getElementById("dy");
-			dx.value = String(points.dx);
-			dy.value = String(points.dy);
-			dx.onchange = function() {
-				points.setSpacing(Number(dx.value), Number(dy.value));
-			};
-			dy.onchange = function() {
-				points.setSpacing(Number(dx.value), Number(dy.value));
-			};
-
-			var max = document.getElementById("max");
-			max.value = String(points.maxFeatures);
-			max.onchange = function() {
-				points.setMaxFeatures(Number(max.value));
-			};
+			// 		var graphic = new OpenLayers.Layer.Image(
+			// 		'scribble_'+k+'',
+			// 		''+path+'/'+scribbles[k]+'',
+			// 		new OpenLayers.Bounds(-200, -150, 200, 150),
+			// 		new OpenLayers.Size(40, 30),
+			// 		{   numZoomLevels: 3,      
+			// 			isBaseLayer: false,
+			// 			opacity: 0.3,
+			// 			displayOutsideMaxExtent: true
+			// 		 });
+			// 		var scribbleLayers[k] = graphic;
+			// 	}
+			// }
 
 
+
+		//  graphic.events.on({
+		//      loadstart: function() {
+		//          OpenLayers.Console.log("loadstart");
+		//      },
+		//      loadend: function() {
+		//          OpenLayers.Console.log("loadend");
+		//      }
+		//  });
+
+
+			// var points = new OpenLayers.Layer.PointGrid({
+			// 	isBaseLayer: false, dx: 15, dy: 15
+			// });
+
+
+			// var graphic = new OpenLayers.Layer.Image( 
+			// 'City Lights', 
+			// ''+path+'/'+scribbles[1367270694]+'', 
+			// new OpenLayers.Bounds(-200, -150, 200, 150), 
+			// new OpenLayers.Size(40, 30), isBaseLayer: false
+			// ); 
+
+			// var graphic2 = new OpenLayers.Layer.Image( 
+			// 'City Lights2', 
+			// ''+path+'/'+scribbles[1367270694]+'', 
+			// new OpenLayers.Bounds(-100, -150, 200, 150), 
+			// new OpenLayers.Size(400, 300), isBaseLayer: false
+			// ); 
+
+			// for (var k in scribbles) {
+			// 	// use hasOwnProperty to filter out keys from the Object.prototype
+			// 	if (scribbles.hasOwnProperty(k)) {
+					
+					// var graphic = new OpenLayers.Layer.Image(
+					// 'scribble_',
+					// ''+path+'/'+scribbles[1367270694]+'',
+					// new OpenLayers.Bounds(-200, -150, 200, 150),
+					// new OpenLayers.Size(40, 30),
+					// {isBaseLayer: false}
+					// );
+					// scribbleLayers[k] = graphic;
+			// 	}
+			// }		
+			// var layer = new OpenLayers.Layer.TMS("Name", "http://example.com/", { 'type':'png', 'getURL':get_my_url });
+			
+			// var map = new OpenLayers.Map({
+			// 	div: "map",
+			// 	layers: [graphic],
+			// 	center: new OpenLayers.LonLat(0, 0),
+			// 	zoom: 1
+			// });
+
+			// extent = new OpenLayers.Bounds(1197980.141, 8101143.032, 1244615.311, 8198123.317); 
+			// map.zoomToExtent(extent); 
+
+			
+
+
+			// maxExtent: new OpenLayers.Bounds(-20037508.3427892,-20037508.3427892,20037508.3427892,20037508.3427892), 
+			// numZoomLevels:18, 
+			// maxResolution:156543.0339, 
+			// units:'m', 
+			// projection: "EPSG:900913",
+			// displayProjection: new OpenLayers.Projection("EPSG:4326")
+
+			// var lonLat = new OpenLayers.LonLat(-100, 40) ;
+			// lonLat.transform(map.displayProjection,map.getProjectionObject());
+			// map.setCenter(lonLat, 5);
+ 
+			// }
+			
+
+	// function get_my_url (bounds) {
+	// 	var res = this.map.getResolution();
+	// 	var x = Math.round ((bounds.left - this.maxExtent.left) / (res * this.tileSize.w));
+	// 	var y = Math.round ((this.maxExtent.top - bounds.top) / (res * this.tileSize.h));
+	// 	var z = this.map.getZoom();
+
+	// 	<?php
+	// 			$sql = "SELECT `tileid`, `AsText(position)` FROM `map`LIMIT 0, 40  ";
+	// 			$result = $mysqli->query($sql);
+	// 			while ($row = $result->fetch_array(MYSQLI_NUM)) {
+	// 			echo 'points['.$row[0]."] = '".$row[1]."';";
+	// 		}
+	// 	?>
+
+	// 	var path = z + "/" + x + "/" + y + "." + this.type; 
+	// 	var url = this.url;
+	// 	if (url instanceof Array) {
+	// 		url = this.selectUrl(path, url);
+	// 	}
+	// 	return url + path;
+
+	// }
+
+
+function map_init() {
+
+	<?php 
+
+	if (!$loggedIn) {
+	exit();
 	}
+		echo "var path = '".root."';";
+		$sql = "SELECT `scribbleid`, `path` FROM `scribbles` LIMIT 0, 40 ";
+		$result = $mysqli->query($sql);
+		while ($row = $result->fetch_array(MYSQLI_NUM)) {
+		echo 'scribbles['.$row[0]."] = '".$row[1]."';";
+	}
+	?>
+	var points = new OpenLayers.Layer.PointGrid({
+		isBaseLayer: true, dx: 15, dy: 15
+	});
+	var graphic = new OpenLayers.Layer.Image(
+									'scribble_',
+									''+path+'/'+scribbles[1367270694]+'',
+									new OpenLayers.Bounds(-20, -15, 20, 15),
+									new OpenLayers.Size(40, 30),
+									{isBaseLayer: false}
+									);
+
+	var map = new OpenLayers.Map({
+		div: "map",
+		layers:[points, graphic],
+		center: new OpenLayers.LonLat(0, 0),
+		zoom: 1
+	});
+}
+	
+
+
 		</script>
 
 		<title>Scribbit - Wall</title>
 	</head>
 
-<body onload="loadScribbles();"> 
+<body onload="map_init()"> 
 			<div id="site">
 				<div id="header">
 					<div id="logo">
@@ -162,15 +238,15 @@ require_once 'header.php';
 								<li><?php echo '<span><a href="'.path.'/gallery">Gallery</a></span>' ?></li>
 								<li><?php echo '<span><a href="'.path.'/wall">Wall</a></span>' ?></li>
 							</ul>
-						</div>	
+						</div>  
 					<div class="searchbar" >
 						<ul>
 							<li><input class="searchtext" type="text" name="searchtext"/></li>
 							<br>
 							<br>
 							<li><input name="filter" type="checkbox" value="favorits"/>Favorits&nbsp;</li>
-							<li><input name="filter" type="checkbox" value="friends">Friends&nbsp;</li>		
-							<li><input name="filter" type="checkbox" value="my"/>Own&nbsp;</li>	
+							<li><input name="filter" type="checkbox" value="friends">Friends&nbsp;</li>     
+							<li><input name="filter" type="checkbox" value="my"/>Own&nbsp;</li> 
 							<li><select name="timefilter">
 								<option value="all">all Time</option> 
 								<option value="h24">last 24 h</option> 
