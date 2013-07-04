8c254260559737ed38ab9171e7b4df8b51af77ce
daired <hady@khalifa.de>
2013-07-04 12:01:06 +0200

    Implemented Drag scroll in wall.php

--- wall.php
+++ wall.php
@@ -2,48 +2,47 @@
 	"http://www.w3.org/TR/html4/strict.dtd">
 <?php 
 require_once 'header.php';
- ?>
+?>
 <html>
-	<head>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-		<meta name="apple-mobile-web-app-capable" content="yes">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<meta name="apple-mobile-web-app-capable" content="yes">
 
-		<?php echo '<link rel="stylesheet" type="text/css" href="'.path.'/ressources/css/headerSearch.css">'; ?>
-		<?php echo '<link rel="stylesheet" type="text/css" href="'.path.'/ressources/css/wall.css">'; ?>
-		<?php echo '<script type="text/javascript" src="'.path.'/ressources/js/jQuery2.js"></script>'; ?>
-		<?php echo '<script type="text/javascript" src="'.path.'/ressources/js/jQueryEvents.js"></script>'; ?>
-		<?php echo '<script type="text/javascript" src="'.path.'/ressources/js/jcanvas.min.js"></script>'; ?>
-		<script type="text/javascript">
+	<?php echo '<link rel="stylesheet" type="text/css" href="'.path.'/ressources/css/headerSearch.css">'; ?>
+	<?php echo '<link rel="stylesheet" type="text/css" href="'.path.'/ressources/css/wall.css">'; ?>
+	<?php echo '<script type="text/javascript" src="'.path.'/ressources/js/jQuery2.js"></script>'; ?>
+	<?php echo '<script type="text/javascript" src="'.path.'/ressources/js/jQueryEvents.js"></script>'; ?>
+	<script type="text/javascript">
 
 
-		var scribbles = {};
-		var positionsx = {};
-		var positionsy = {};
-		var map={};
-		
-		var temp_scribbles = {};
-		var temp_positionsx = {};
-		var temp_positionsy = {};
-		var temp_map={};
+	var scribbles = {};
+	var positionsx = {};
+	var positionsy = {};
+	var map={};
 
-		var canvasPos ={};
-		var canvases ={};
-		var topY, bottomY, leftX, rightX;
-		var mapCells = {};
+	var temp_scribbles = {};
+	var temp_positionsx = {};
+	var temp_positionsy = {};
+	var temp_map={};
+
+	var canvasPos ={};
+	var canvases ={};
+	var topY, bottomY, leftX, rightX;
+	var mapCells = {};
 		// var lastX, curX, lastY, curY;
 
 		<?php 
 
-			if (!$loggedIn) {
+		if (!$loggedIn) {
 			exit();
-			}
-			else {
-				echo "var path = '".path."';".PHP_EOL;
-				echo "var root = '".root."';".PHP_EOL; 
-				echo "var xpos = ".$xpos.";".PHP_EOL;
-				echo "var ypos = ".$ypos.";".PHP_EOL;
-				echo "var zoom = ".$zoom.";".PHP_EOL;
+		}
+		else {
+			echo "var path = '".path."';".PHP_EOL;
+			echo "var root = '".root."';".PHP_EOL; 
+			echo "var xpos = ".$xpos.";".PHP_EOL;
+			echo "var ypos = ".$ypos.";".PHP_EOL;
+			echo "var zoom = ".$zoom.";".PHP_EOL;
 				// $sql = "SELECT `scribbleid`, `path` FROM `scribbles` LIMIT 0, 40 ";
 				// $result = $mysqli->query($sql);
 				// while ($row = $result->fetch_array(MYSQLI_NUM)) {
@@ -54,22 +53,22 @@ require_once 'header.php';
 				// while ($row = $result->fetch_array(MYSQLI_NUM)) {
 				// 	echo 'points['.$row[0]."] = '".$row[1]."';";
 				// }
-				$bl = ($xpos-9).' '.($ypos-11);
-				$tl = ($xpos-9).' '.($ypos+6);
-				$tr = ($xpos+17).' '.($ypos+6);
-				$br = ($xpos+17).' '.($ypos-11);
+			$bl = ($xpos-9).' '.($ypos-11);
+			$tl = ($xpos-9).' '.($ypos+6);
+			$tr = ($xpos+17).' '.($ypos+6);
+			$br = ($xpos+17).' '.($ypos-11);
 
-				$sql = sprintf("SELECT `scribbles`.`scribbleid`, `scribbles`.`path`, `scribbles`.`userid`, `scribbles`.`creation`, X(`map`.`position`), Y(`map`.`position`) FROM `scribbles`, `map` WHERE `scribbles`.`scribbleid` = `map`.`scribbleid` AND MBRContains(GeomFromText('Polygon((%s, %s, %s, %s, %s))'), `map`.`position`) = 1 LIMIT 0, 486 ", $bl, $br, $tr, $tl, $bl);
-				$result = $mysqli->query($sql);
-				while ($row = $result->fetch_array()) {
-					echo 'scribbles['.$row[0]."] = '/scribbles/l/".$row[1]."'; ";
+			$sql = sprintf("SELECT `scribbles`.`scribbleid`, `scribbles`.`path`, `scribbles`.`userid`, `scribbles`.`creation`, X(`map`.`position`), Y(`map`.`position`) FROM `scribbles`, `map` WHERE `scribbles`.`scribbleid` = `map`.`scribbleid` AND MBRContains(GeomFromText('Polygon((%s, %s, %s, %s, %s))'), `map`.`position`) = 1 LIMIT 0, 486 ", $bl, $br, $tr, $tl, $bl);
+			$result = $mysqli->query($sql);
+			while ($row = $result->fetch_array()) {
+				echo 'scribbles['.$row[0]."] = '/scribbles/l/".$row[1]."'; ";
 					// echo 'scribDates['.$row[0]."] = '".($row[3])."'; ";
-					echo 'positionsx['.$row[0]."] = '".($row[4])."'; ";
-					echo 'positionsy['.$row[0]."] = '".($row[5])."'; ";
-					echo 'map['.$row[4].''.$row[5]."] = '".$row[0]."'; ".PHP_EOL;
-				}
-
+				echo 'positionsx['.$row[0]."] = '".($row[4])."'; ";
+				echo 'positionsy['.$row[0]."] = '".($row[5])."'; ";
+				echo 'map['.$row[4].''.$row[5]."] = '".$row[0]."'; ".PHP_EOL;
 			}
+
+		}
 		?>
 
 
@@ -78,10 +77,14 @@ require_once 'header.php';
 					/////////////////////////////
 
 		$(document).ready(function() {
-				topY =  (ypos+6);
-				bottomY = (ypos-11);
-				leftX = (xpos-9);
-				rightX = (xpos+17);
+						topY =  (ypos+6);
+						bottomY = (ypos-11);
+						leftX = (xpos-9);
+						rightX = (xpos+17);
+
+				// $('#ithingy').overscroll();
+				draginit();
+				
 
 				//TODO FOKUS AUF XPOS, YPOS
 				window.scrollTo(($(document).width() - $(window).width())/2, ($(document).height() - $(window).height())/2);
@@ -97,56 +100,107 @@ require_once 'header.php';
 					}	
 				}
 
+				$(document).on("mousedown", "#divCanvas", function(event){
+					event.preventDefault();
+					
+					dragstart(document.getElementById("divCanvas"));
+				})
+
+				
 				
 				$(document).on("click", ".scribble", function(event){
-					console.log("click "+ event.target.id + " zoomlvl"+zoom);
+					
 					event.preventDefault();
-					var topOff = $(this).focus().offset().top, $w = $(window);
-					var leftOff = $(this).focus().offset().left, $w = $(window);
-					$w.scrollTop(topOff - ($w.height() / 2));
-					$w.scrollLeft(leftOff - ($w.width()  / 2));
+					// var topOff = $(this).focus().offset().top, $w = $(window);
+					// var leftOff = $(this).focus().offset().left, $w = $(window);
+					// $w.scrollTop(topOff - ($w.height() / 2));
+					// $w.scrollLeft(leftOff - ($w.width()  / 2));
 
 					$(this).stop().animate({
 						
 						
 						
-						}, 1000, function() {
+					}, 1000, function() {
 							// Animation complete.
-					});
+						});
 					
 				});
-			
+
 				$(document).on("mouseenter", ".scribble", function(event){
-							console.log("mouseenter "+ event.target.id + " zoomlvl"+zoom);
+					
 					event.preventDefault();
 					$(this).stop().animate({
 						opacity: 1
 						
 						
-						}, 300, function() {
+					}, 300, function() {
 							// Animation complete.
-					});
+						});
 				});
 
 				$(document).on("mouseleave", ".scribble", function(event){
-							console.log("mouseleave "+ event.target.id + " zoomlvl"+zoom);
+					
 					event.preventDefault();
 
 					$(this).stop().animate({
 						opacity: 0.75
 						
 						
-						}, 300, function() {
+					}, 300, function() {
 							// Animation complete.
-					});
+						});
 				});
 
 
-		})
+			})
 
-		
-		$(window).scroll(function()
-			{
+//Das Objekt, das gerade bewegt wird.
+var dragobjekt = null;
+
+// Position, an der das Objekt angeklickt wurde.
+var dragx = 0;
+var dragy = 0;
+
+// Mausposition
+var posx = 0;
+var posy = 0;
+
+
+function draginit() {
+ // Initialisierung der Überwachung der Events
+
+  document.onmousemove = drag;
+  document.onmouseup = dragstop;
+}
+
+
+function dragstart(element) {
+   //Wird aufgerufen, wenn ein Objekt bewegt werden soll.
+
+  dragobjekt = element;
+  dragx = posx - dragobjekt.offsetLeft;
+  dragy = posy - dragobjekt.offsetTop;
+}
+
+
+function dragstop() {
+  //Wird aufgerufen, wenn ein Objekt nicht mehr bewegt werden soll.
+
+  dragobjekt=null;
+
+}
+
+
+function drag(event) {
+  posx = document.all ? window.event.clientX : event.pageX;
+  posy = document.all ? window.event.clientY : event.pageY;
+  if(dragobjekt != null) {
+    window.scrollBy((-(posx - dragx)), (-(posy - dragy)));
+  }
+}
+
+$(window).scroll(function()
+{
 				// Scrollbar on Bottom
 				if($(window).scrollTop() == $(document).height() - $(window).height())
 				{
@@ -155,27 +209,27 @@ require_once 'header.php';
 						type: "POST",
 						url: "wallAjaxRequest.php",
 						data: {	bl: leftX+' '+(bottomY-7), 
-								br: rightX+' '+(bottomY-7),
-								tr: rightX+' '+(bottomY-1), 
-								tl: leftX+' '+(bottomY-1) 
-							},
-						success: function(data){
-								$('div#bottomProcessBar').hide();
-								var json = $.parseJSON(data);
-								if($.isEmptyObject(json.temp_scribbles)){
+						br: rightX+' '+(bottomY-7),
+						tr: rightX+' '+(bottomY-1), 
+						tl: leftX+' '+(bottomY-1) 
+					},
+					success: function(data){
+						$('div#bottomProcessBar').hide();
+						var json = $.parseJSON(data);
+						if($.isEmptyObject(json.temp_scribbles)){
 
+						}
+						else {
+							addBottomRow();
+							for(var k in json.temp_scribbles){
+								if (json.temp_scribbles.hasOwnProperty(k) && scribbles[k] == null){
+									scribbles[k] = json.temp_scribbles[k];
+									createScribble(json.temp_positionsx[k], json.temp_positionsy[k], k);
 								}
-								else {
-									addBottomRow();
-									for(var k in json.temp_scribbles){
-										if (json.temp_scribbles.hasOwnProperty(k) && scribbles[k] == null){
-											scribbles[k] = json.temp_scribbles[k];
-											createScribble(json.temp_positionsx[k], json.temp_positionsy[k], k);
-										}
-									}	
-								}
-							}
-						});
+							}	
+						}
+					}
+				});
 				}
 
 				// Scrollbar on Top
@@ -185,25 +239,25 @@ require_once 'header.php';
 						type: "POST",
 						url: "wallAjaxRequest.php",
 						data: {	bl: leftX+' '+(topY+1), 
-								br: rightX+' '+(topY+1),
-								tr: rightX+' '+(topY+7), 
-								tl: leftX+' '+(topY+7) 
-							},
-						success: function(data){
-								$('div#topProcessBar').hide();
-								var json = $.parseJSON(data);
-								if($.isEmptyObject(json.temp_scribbles)){
+						br: rightX+' '+(topY+1),
+						tr: rightX+' '+(topY+7), 
+						tl: leftX+' '+(topY+7) 
+					},
+					success: function(data){
+						$('div#topProcessBar').hide();
+						var json = $.parseJSON(data);
+						if($.isEmptyObject(json.temp_scribbles)){
 									//		
 								}
 								else {
-										addTopRow();
-										$(window).scrollTop($(window).scrollTop()+(6*$(".mapCell").height()));
-										for(var k in json.temp_scribbles){
-											if (json.temp_scribbles.hasOwnProperty(k) && scribbles[k] == null){
-												scribbles[k] = json.temp_scribbles[k];
-												createScribble(json.temp_positionsx[k], json.temp_positionsy[k], k);
-											}
-										}	
+									addTopRow();
+									$(window).scrollTop($(window).scrollTop()+(6*$(".mapCell").height()));
+									for(var k in json.temp_scribbles){
+										if (json.temp_scribbles.hasOwnProperty(k) && scribbles[k] == null){
+											scribbles[k] = json.temp_scribbles[k];
+											createScribble(json.temp_positionsx[k], json.temp_positionsy[k], k);
+										}
+									}	
 								}
 							}
 						});
@@ -218,82 +272,82 @@ require_once 'header.php';
 						type: "POST",
 						url: "wallAjaxRequest.php",
 						data: {	bl: (leftX-10)+' '+bottomY, 
-								br: (leftX-1)+' '+bottomY,
-								tr: (leftX-1)+' '+topY, 
-								tl: (leftX-10)+' '+topY 
-							},
-						success: function(data){
-								$('div#leftProcessBar').hide();
-								var json = $.parseJSON(data);
-								if($.isEmptyObject(json.temp_scribbles)){
+						br: (leftX-1)+' '+bottomY,
+						tr: (leftX-1)+' '+topY, 
+						tl: (leftX-10)+' '+topY 
+					},
+					success: function(data){
+						$('div#leftProcessBar').hide();
+						var json = $.parseJSON(data);
+						if($.isEmptyObject(json.temp_scribbles)){
 
+						}
+						else {
+							addLeftColumn();
+							$(window).scrollLeft($(window).scrollLeft()+(9*$(".mapCell").width()));
+							for(var k in json.temp_scribbles){
+								if (json.temp_scribbles.hasOwnProperty(k) && scribbles[k] == null){
+									scribbles[k] = json.temp_scribbles[k];
+									createScribble(json.temp_positionsx[k], json.temp_positionsy[k], k);
 								}
-								else {
-									addLeftColumn();
-									$(window).scrollLeft($(window).scrollLeft()+(9*$(".mapCell").width()));
-									for(var k in json.temp_scribbles){
-										if (json.temp_scribbles.hasOwnProperty(k) && scribbles[k] == null){
-											scribbles[k] = json.temp_scribbles[k];
-											createScribble(json.temp_positionsx[k], json.temp_positionsy[k], k);
-										}
-									}	
-								}
-							}
-						});
+							}	
+						}
+					}
+				});
 				}
 
 				// Scrollbar Right
 				else if($(window).scrollLeft() == $(document).width() - $(window).width()){
-						
+
 					$('div#rightProcessBar').show();
 					$.ajax({
 						type: "POST",
 						url: "wallAjaxRequest.php",
 						data: {	bl: (rightX+1)+' '+bottomY, 
-								br: (rightX+10)+' '+bottomY,
-								tr: (rightX+10)+' '+topY, 
-								tl: (rightX+1)+' '+topY 
-							},
-						success: function(data){
-								$('div#rightProcessBar').hide();
-								var json = $.parseJSON(data);
-								if($.isEmptyObject(json.temp_scribbles)){
+						br: (rightX+10)+' '+bottomY,
+						tr: (rightX+10)+' '+topY, 
+						tl: (rightX+1)+' '+topY 
+					},
+					success: function(data){
+						$('div#rightProcessBar').hide();
+						var json = $.parseJSON(data);
+						if($.isEmptyObject(json.temp_scribbles)){
 
+						}
+						else {
+							addRightColumn();
+							for(var k in json.temp_scribbles){
+								if (json.temp_scribbles.hasOwnProperty(k) && scribbles[k] == null){
+									scribbles[k] = json.temp_scribbles[k];
+									createScribble(json.temp_positionsx[k], json.temp_positionsy[k], k);
 								}
-								else {
-									addRightColumn();
-									for(var k in json.temp_scribbles){
-										if (json.temp_scribbles.hasOwnProperty(k) && scribbles[k] == null){
-											scribbles[k] = json.temp_scribbles[k];
-											createScribble(json.temp_positionsx[k], json.temp_positionsy[k], k);
-										}
-									}	
-								}
-							}
-						});
+							}	
+						}
+					}
+				});
 				}
 			})
 
 
-		function isScrolledIntoView(elem)
-		{
-			var docViewTop = $(window).scrollTop();
-			var docViewBottom = docViewTop + $(window).height();
+	function isScrolledIntoView(elem)
+{
+	var docViewTop = $(window).scrollTop();
+	var docViewBottom = docViewTop + $(window).height();
 
-			var elemTop = $(elem).offset().top;
-			var elemBottom = elemTop + $(elem).height();
+	var elemTop = $(elem).offset().top;
+	var elemBottom = elemTop + $(elem).height();
 
-			return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-		}
+	return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
 
-		function scrollToPosition(x,y){
+function scrollToPosition(x,y){
 
-		}
+}
 
 
-		function createScribble(x, y, scrid){	
-			$('<div/>', {
-				id: 'scribble'+x+'_'+y+''
+function createScribble(x, y, scrid){	
+	$('<div/>', {
+		id: 'scribble'+x+'_'+y+''
 				// title: 'Become a Googler',
 				// rel: 'external',
 				// text: 'Go to Google!'
@@ -338,7 +392,7 @@ require_once 'header.php';
 			).insertAfter('#mapCell'+(x-1)+'_'+y);
 			store2DArray('mapCell'+x+'_'+y+'', x, y, mapCells)
 		}
-	
+
 		function createLeftMapCell(x,y){		
 			$('<div/>', {
 				id: 'mapCell'+x+'_'+y+''
@@ -400,7 +454,7 @@ require_once 'header.php';
 		}
 
 
-	
+
 		function store2DArray(id, xVal, yVal, array){
 			if (typeof array[xVal] == "undefined"){
 				array[xVal] = new Array();
@@ -414,19 +468,19 @@ require_once 'header.php';
 		// erst dividieren, dann auf Ganzzahl abrunden. 
 		if ( n1*n2 > 0 ) return Math.floor( n1/n2 );
 		else return Math.ceil ( n1/n2 );
-		}
-		
-
-
-
- 
+	}
 
 
 
 
 
 
-			
+
+
+
+
+
+
 
 			// //************************************************************************
 			// function mousedown(evt)
@@ -480,7 +534,7 @@ require_once 'header.php';
 			//		}
 
 			//		//console.log("pressure: " + pressure);
-						
+
 			//		curX = (ev.pageX?ev.pageX : ev.clientX + document.body.scrollLeft) - canvasPos.x;
 			//		curY = (ev.pageY?ev.pageY : ev.clientY + document.body.scrollTop ) - canvasPos.y;
 
@@ -508,25 +562,25 @@ require_once 'header.php';
 			//		lastY = curY;
 			// }
 
-		</script>
+			</script>
 
-		<title>Scribbit - Wall</title>
-	</head>
+			<title>Scribbit - Wall</title>
+		</head>
 
-	<body> 
-		<div id="leftProcessBar" class="rotating"></div>
+		<body> 
+			<div id="leftProcessBar" class="rotating"></div>
 			<div id="rightProcessBar" class="rotating"></div>
 			<div id="topProcessBar" class="rotating"></div>
 			<div id="bottomProcessBar" class="rotating"></div>
-		<div id="site">
-			<div id="header">
-				<?php include docroot.'/'.path.'/topnav.php'; ?>
-			</div>	
+			<div id="site">
+				<div id="header">
+					<?php include docroot.'/'.path.'/topnav.php'; ?>
+				</div>	
 
-			<div id="clippingMask">	
-			
-				<div id="divCanvas"></div>
+				<div id="clippingMask">	
+
+					<div id="divCanvas" class="dragger"></div>
+				</div>
 			</div>
-		</div>
-	</body>
-</html>
+		</body>
+		</html>
